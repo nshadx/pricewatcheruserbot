8eaeb9381f8f453979ce57f9b9b843d7f234caa7
nkorabelnikov <korabelnikov@rza.ru>
2025-12-19 18:40:07 +0300

    add routing

--- pricewatcheruserbot/Program.cs
+++ pricewatcheruserbot/Program.cs
@@ -1,7 +1,44 @@
+using Microsoft.Extensions.Options;
 using pricewatcheruserbot;
+using pricewatcheruserbot.Configuration;
+using TL;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.Configure<BotCredentials>(builder.Configuration.GetRequiredSection(nameof(BotCredentials)));
+builder.Services.AddSingleton<UpdateRouter>();
+builder.Services.AddSingleton(provider =>
+{
+    var botCredentials = provider.GetRequiredService<IOptions<BotCredentials>>().Value;
+    
+    var client = new WTelegram.Client(Config);
+    
+    string? Config(string what)
+    {
+        switch (what)
+        {
+            case "api_id": return botCredentials.ApiId;
+            case "api_hash": return botCredentials.ApiHash;
+            case "password": return botCredentials.Password;
+            case "phone_number": return botCredentials.PhoneNumber;
+            case "verification_code": Console.Write("Verification code: "); return Console.ReadLine();
+            case "first_name": throw new NotImplementedException();
+            case "last_name": throw new NotImplementedException();
+            default: return null;
+        }
+    }
+    
+    return client;
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
-host.Run();
+
+var client = host.Services.GetRequiredService<WTelegram.Client>();
+var updateRouter = host.Services.GetRequiredService<UpdateRouter>();
+
+_ = await client.LoginUserIfNeeded();
+_ = client.WithUpdateManager(updateRouter.Handle);
+
+await host.RunAsync();
--- pricewatcheruserbot/Worker.cs
+++ pricewatcheruserbot/Worker.cs
@@ -1,17 +1,9 @@
 namespace pricewatcheruserbot;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker : BackgroundService
 {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (logger.IsEnabled(LogLevel.Information))
-            {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-
-            await Task.Delay(1000, stoppingToken);
-        }
+        return Task.CompletedTask;
     }
 }
